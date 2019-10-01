95698dc30be1798e0ba5ade02850af9791bf58e0
ajcper <james@perlego.com>
2019-10-01 08:40:13 +0100

    adding standard styled component imports

--- src/exportable/components/atoms/NewOne/styled/StyledNewOne.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "./../../../../../utils/styled-components";
-
-const StyledNewOne = styled<{}, "div">("div")`
-  color: ${({ theme }) => theme.colors.primaryColor};
-`;
-
-export default StyledNewOne;
